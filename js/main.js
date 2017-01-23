4ba3228f0da6e82454ea02990b6eb71acda6dd83
Оксана <o.rozsokha@gmail.com>
2017-01-23 23:59:33 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -7,5 +7,24 @@ $(document).ready(function () {
         columnWidth: 200,
         fitWidth: true
     });
+    // $(".owl-carousel").owlCarousel();
 
+
+    // jQuery(window).load(function(){
+    //     $('.slider').fractionSlider({
+    //         backgroundAnimation: true,
+    //         backgroundY: 100,
+    //         backgroundX: 250
+    //     });
+    // });
+
+    $('.slider').slick({
+        // dots: true,
+        // infinite: true,
+        // speed: 500,
+        // fade: true,
+        // cssEase: 'linear'
+    });
 });
+
+
