d25103270d086744c20f4fc6f2dff7d122e5088a
Оксана <o.rozsokha@gmail.com>
2017-01-24 23:11:31 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
 
 $(document).ready(function () {
 
+
+    $('.slider').slick({
+        dots: true,
+        initialSlide: 1,
+        arrows: true
+    });
+
+    $('.design-list li').mouseover(function () {
+       $(this).children('.jq-text').find('p').text('Redesigning width personality');
+    });
+    $('.design-list li').mouseout(function () {
+        $(this).children('.jq-text').find('p').text('Lorem ipsum dolor sit amet,consectetur.');
+    });
+
     $('.grid').masonry({
         // options
         itemSelector: '.grid-item',
         columnWidth: 200,
         fitWidth: true
     });
-    // $(".owl-carousel").owlCarousel();
-
-
-    // jQuery(window).load(function(){
-    //     $('.slider').fractionSlider({
-    //         backgroundAnimation: true,
-    //         backgroundY: 100,
-    //         backgroundX: 250
-    //     });
-    // });
-
-    $('.slider').slick({
-        // dots: true,
-        // infinite: true,
-        // speed: 500,
-        // fade: true,
-        // cssEase: 'linear'
+    $('.clients-list').slick({
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 2,
+        arrows: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
+
 });
 
 
